22c34323e51e9514ec8daef6a30a6a2b4067ac87
Adrian Gehrig <40770796+AdrianGehrig@users.noreply.github.com>
2019-01-23 18:09:53 +0100

    added Plant measurements to Matlab and ident toolbox

--- System Identification/Messungen Dersch/Copy_of_plot2019_01_22.m
+++ System Identification/Messungen Dersch/Copy_of_plot2019_01_22.m
@@ -1,5 +1,5 @@
 figure
-p1=subplot(4,1,1)
+p1=subplot(5,1,1)
 area(DatumUhrzeit,BLHA0)
 legend("Hand=1 Automatik=0",'Location','northwest')
 axis tight
@@ -14,8 +14,8 @@ abstand=1/5
 hoehe=1/6
 
 
-p2=subplot(4,1,2)
-plot(DatumUhrzeit,BL,'b',DatumUhrzeit,Kohle,'k', DatumUhrzeit,Gas,'y', 'LineWidth',2)
+p2=subplot(5,1,2)
+plot(DatumUhrzeit,BL,'b',DatumUhrzeit,Kohle,'k', DatumUhrzeit,Gas,'g', 'LineWidth',2)
 grid on
 legend("BL [%]","Kohle [%]","Gas [%]",'Location','west' )
 axis tight
@@ -24,31 +24,38 @@ pos(2) =offset-abstand
 pos(4) =hoehe                        % Increase height.
 set( p2(1), 'Position', pos ) ;
 
-% p3=subplot(4,1,3)
-% plot(DatumUhrzeit,Aufgabe,'LineWidth',2)
-% grid on
-% legend("Aufgabe",'Location','southwest')
-
-p3=subplot(4,1,3)
-plot(DatumUhrzeit,VBLSollNmh,DatumUhrzeit,VBLIstNmh,'LineWidth',2)
+p3=subplot(5,1,3)
+plot(DatumUhrzeit,KohleISTKgh,DatumUhrzeit,KohleSOLLKgh,'k')
+legend("Kohle ist", "Kohle soll",'Location','southwest')
 grid on
-legend("VBL Soll [Nmh]","VBL Ist [Nmh]",'Location','southwest')
+
 axis tight
-pos = get( p3(1), 'Position' );                    
+pos = get( p3(1), 'Position' );                
 pos(2) =offset-2*abstand
 pos(4) =hoehe                        % Increase height.
 set( p3(1), 'Position', pos ) ;
 
-p4=subplot(4,1,4)
-plot(DatumUhrzeit,BLSOLLC,DatumUhrzeit,BLISTC,'LineWidth',2)
+
+p4=subplot(5,1,4)
+plot(DatumUhrzeit,VBLSollNmh,DatumUhrzeit,VBLIstNmh,'LineWidth',2)
 grid on
-legend("Bl soll [°C]","Bl ist [°C]",'Location','southwest')
+legend("VBL Soll [Nmh]","VBL Ist [Nmh]",'Location','southwest')
 axis tight
-pos = get( p4(1), 'Position' );                  
+pos = get( p4(1), 'Position' );                    
 pos(2) =offset-3*abstand
 pos(4) =hoehe                        % Increase height.
 set( p4(1), 'Position', pos ) ;
 
+p5=subplot(5,1,5)
+plot(DatumUhrzeit,BLSOLLC,DatumUhrzeit,BLISTC,'LineWidth',2)
+grid on
+legend("Bl soll [°C]","Bl ist [°C]",'Location','southwest')
+axis tight
+pos = get( p5(1), 'Position' );                  
+pos(2) =offset-4*abstand
+pos(4) =hoehe                        % Increase height.
+set( p5(1), 'Position', pos ) ;
 
 
-linkaxes([p1,p2,p3,p4],'x')
+
+linkaxes([p1,p2,p3,p4,p5],'x')
