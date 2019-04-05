176d3f7d1c808d6479e34d3d219181d6e11f424a
Adrian Gehrig <40770796+AdrianGehrig@users.noreply.github.com>
2019-04-05 16:36:22 +0200

    freitag bei mrac

--- Adaptive Control and MPC/MIAC/Messungen_live/real_plot.m
+++ Adaptive Control and MPC/MIAC/Messungen_live/real_plot.m
@@ -1,9 +1,17 @@
-p1=subplot(2,1,1)
-plot(actual_valueTime,actual_valueValueY,actual_valueTime,SetpointValueY)
+figure
+p1=subplot(3,1,1)
+plot(actual_valueTime,actual_valueValueY,actual_valueTime,SetpointValueY,'LineWidth',2)
+legend('actual_ valueValue','SetpointValue')
 grid on
 
-p2=subplot(2,1,2)
-plot(actual_valueTime,outputValueY)
+p2=subplot(3,1,2)
+plot(actual_valueTime,outputValueY, 'LineWidth',2)
+legend('outputValue')
 grid on
 
-linkaxes([p1,p2],'x')
+p3=subplot(3,1,3)
+plot(actual_valueTime,GainFilteredValueY,actual_valueTime,GainNotFilteredValueY,'LineWidth',2)
+legend('GainFilteredValue','GainNotFilteredValue')
+grid on
+
+linkaxes([p1,p2,p3],'x')
